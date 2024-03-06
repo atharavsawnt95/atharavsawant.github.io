b80442b84a335e5b472704ff95f0b9537c6f64ba
atharavsawnt95 <atharavsawnt95@gmail.com>
2024-03-06 16:29:28 +0530

    add

---
+++ function.js
@@ -0,0 +1,22 @@
+var Number1=20;
+var Number2=20;
+
+function addition(){
+    console.log("Addition is ",Number1+Number2);
+}
+addition();
+
+function Subtraction(){
+    console.log("Subtraction is ",Number1-Number2);
+}
+Subtraction();
+
+function multiplication(){
+    console.log("Multiplication is ",Number1*Number2);
+}
+multiplication();
+
+function division(){
+    console.log("Division is ",Number1/Number2);
+}
+division();
---
+++ script.js
@@ -0,0 +1,14 @@
+var value1=10;
+var value2=20;
+var value3=value1+value2;
+var value4=value1-value2;
+var value5=value1/value2;
+var value6=value1*value2;
+
+
+console.log(value3);
+console.log(value4);
+console.log(value5);
+console.log(value6);
+
+
